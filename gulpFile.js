ad55e2ba65b1e540d06e4bc1b5773cbad00b8b08
Fa Shishir <fashishir4@gmail.com>
2020-02-23 18:14:38 +0600

    update

--- gulpFile.js
+++ gulpFile.js
@@ -1,6 +1,11 @@
 
 var gulp = require('gulp'),
- watch = require('gulp-watch');
+    watch = require('gulp-watch'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    cssvars = require('postcss-simple-vars'),
+    nested = require('postcss-nested'),
+    cssImport = require('postcss-import');
 
 gulp.task('default', function () {
     console.log("hurray ...we create a gulp task.");
@@ -11,19 +16,21 @@ gulp.task('html', function () {
 });
 
 gulp.task('styles', function () {
-    console.log("somethink about sass or postCss etc ");
+    return gulp.src('./app/assets/styles/styles.css')
+        .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+        .pipe(gulp.dest('./app/temp/styles'));
 });
 
-gulp.task('watch',  function(){
+gulp.task('watch', function () {
 
-watch('./app/index.html',  function(){
-gulp.start('html');
+    watch('./app/index.html', function () {
+        gulp.start('html');
 
-});
+    });
 
-watch('./app/assets/styles/**/*.css', function(){
+    watch('./app/assets/styles/**/*.css', function () {
 
-gulp.start('styles');
-});
+        gulp.start('styles');
+    });
 
 });
